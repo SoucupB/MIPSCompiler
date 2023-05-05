ec31a34d8559cb3f9ac348b752291d36e1307b25
unknown <bogdan.soucup@gmail.com>
2023-05-05 20:53:13 +0300

    WIP

---
+++ index.js
@@ -0,0 +1,28 @@
+const MipsAst = require('./mips_ast');
+
+const codeToCompile = `
+int7_t ana = 5
+int7_t vasile = 6
+
+int7_t ionel;
+int7_t i;
+for(i = 0; i < ana; i++) {
+  ionel = ionel + vasile;
+}
+`
+
+
+
+const mips = new MipsAst({
+  addition: 0,
+  multiplication: 1,
+  logical_or: 2,
+  logical_and: 3,
+  engine: 'x16',
+  code: codeToCompile
+})
+
+console.log(mips.compile())
+
+
+
---
+++ mips_ast.js
@@ -0,0 +1,11 @@
+class MipsAst {
+  construct(config) {
+    this.config = config;
+  }
+
+  compile() {
+    return 'test'
+  }
+}
+
+module.exports = { MipsAst }
