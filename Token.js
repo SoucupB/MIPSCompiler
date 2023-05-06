7363a87bb7863dc8ee7fde773d55d4bf1cf1b941
SoucupB <bogdan.soucup@gmail.com>
2023-05-06 12:55:54 +0300

    Add token

--- Token.js
+++ Token.js
@@ -1,9 +1,74 @@
 const tokens = {
   int7_t: 0,
-  variable: 1,
+  undefined_variable: 1,
   for: 2,
   if: 3,
   open_paranth: 4,
   close_paranth: 5,
-  bracket: 6
+  semicolon: 6,
+  equal: 7,
+  double_equal: 8,
+  expression: 9,
+  defined_variable: 10,
+  sign: 11,
+  open_bracket: 12,
+  close_bracket: 13
+}
+
+const newRowExpectation = {
+  int7_t: [  // (int7_t) (variable) = (test + 1)(;)
+    {
+      'undefined_variable': [
+        {
+          'equal': [
+            {
+              'expression': [
+                {
+                  'semicolon': []
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  defined_variable: [ // (defined_variable) = (test + 1)(;)
+    {
+      'equal': [
+        {
+          'expression': [
+            {
+              'semicolon': []
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  if: [
+    {
+      'open_paranth': [
+        {
+          'expression': [
+            {
+              'close_paranth': [
+                {
+                  'open_bracket': [
+                    {
+                      'code_segment': [
+                        {
+                          'close_bracket': []
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
 }
