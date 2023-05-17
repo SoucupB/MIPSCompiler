3b5640470004d6e87ee3610ea477e91534af1231
SoucupB <bogdan.soucup@gmail.com>
2023-05-17 23:33:09 +0300

    Add compiler register

--- Compiler.test.js
+++ Compiler.test.js
@@ -1,5 +1,5 @@
 const ExpressionTree = require('./ExpressionTree');
-const { RegisterEmbed } = require('./Register');
+const { Register, RegisterEmbed, RegistersEmbed } = require('./Register');
 const { tokens } = require('./Token')
 const Compiler = require('./Compiler')
 const { Utils } = require('./Utils')
@@ -261,7 +261,7 @@ test('conditional expression v3', () => {
 test('loop expression v1', () => {
   const toCompile = [
     Utils.createInitializationPayload(['int7_t', 'c']),
-    Utils.createInitializationPayload(['int7_t', 'b', '1']), // second param should not free registers.
+    Utils.createInitializationPayload(['int7_t', 'b', '1']),
     Utils.createForLoopPayload([Utils.createAssignationPayload(['c', '0']), Utils.createExpression(['c', '<', '5']), Utils.createAssignationPayload(['c', 'c', '+', '1'])], [
       Utils.createAssignationPayload(['b', 'b', '+', '1']),
     ]),
@@ -292,4 +292,42 @@ test('loop expression v1', () => {
     new RegisterEmbed('add', [6,3,4]),
     new RegisterEmbed('mov', ["[2]",6]),
   ])).toBe(true)
+});
+
+test('loop expression v2', () => {
+  const toCompile = [
+    Utils.createInitializationPayload(['int7_t', 'c']),
+    Utils.createInitializationPayload(['int7_t', 'b', '1']),
+    Utils.createForLoopPayload([Utils.createAssignationPayload(['c', '0']), Utils.createExpression(['c', '+', '1', '<', '5']), Utils.createAssignationPayload(['c', 'c', '+', '3'])], [
+      Utils.createAssignationPayload(['b', 'b', '+', '1']),
+    ]),
+    Utils.createInitializationPayload(['int7_t', 'a', '5', '+', 'b'])
+  ]
+  const code = new Compiler(toCompile);
+  code.compile()
+  const resp = new RegistersEmbed(code.asm).toMips();
+  console.log(code.asm, resp)
+  // expect(Utils.areRegistersEqual(code.asm, [
+  //   new RegisterEmbed('mov', [3,1]),
+  //   new RegisterEmbed('mov', ["[1]",3]),
+  //   new RegisterEmbed('mov', [3,0]),
+  //   new RegisterEmbed('mov', ["[0]",3]),
+  //   new RegisterEmbed('mov', [3,"[0]"]),
+  //   new RegisterEmbed('mov', [4,5]),
+  //   new RegisterEmbed('slt', [5,4,3]),
+  //   new RegisterEmbed('benq', [5,1,9]),
+  //   new RegisterEmbed('mov', [3,"[1]"]),
+  //   new RegisterEmbed('mov', [4,1]),
+  //   new RegisterEmbed('add', [6,3,4]),
+  //   new RegisterEmbed('mov', ["[1]",6]),
+  //   new RegisterEmbed('mov', [3,"[0]"]),
+  //   new RegisterEmbed('mov', [4,1]),
+  //   new RegisterEmbed('add', [6,3,4]),
+  //   new RegisterEmbed('mov', ["[0]",6]),
+  //   new RegisterEmbed('j', [4]),
+  //   new RegisterEmbed('mov', [3,5]),
+  //   new RegisterEmbed('mov', [4,"[1]"]),
+  //   new RegisterEmbed('add', [6,3,4]),
+  //   new RegisterEmbed('mov', ["[2]",6]),
+  // ])).toBe(true)
 });
--- Register.js
+++ Register.js
@@ -35,4 +35,88 @@ class RegisterEmbed {
   }
 }
 
-module.exports = { Register, RegisterEmbed };
+class RegistersEmbed {
+  constructor(asm) {
+    this.asm = asm
+  }
+
+  _isRegister(param) {
+    return typeof param === 'number';
+  }
+
+  _isMem(param) {
+    return typeof param !== 'number' && param[0] == '[';
+  }
+
+  _isNumber(param) {
+    return typeof param === 'number';
+  }
+
+  _extractMemory(param) {
+    return parseInt(param.substring(1, param.length - 1));
+  }
+
+  _registerMove(params) {
+    if(this._isRegister(params[0]) && this._isNumber(params[1])) {
+      return new RegisterEmbed('addi', [`$${params[0]}`, this.zeroReg, params[1]])
+    }
+    if(this._isRegister(params[0]) && this._isMem(params[1])) {
+      return new RegisterEmbed('lw', [`$${params[0]}`, this.zeroReg, this._extractMemory(params[1])])
+    }
+    if(this._isMem(params[0]) && this._isRegister(params[1])) {
+      return new RegisterEmbed('sw', [`$${params[1]}`, this.zeroReg, this._extractMemory(params[0])])
+    }
+    return null;
+  }
+
+  toMips() {
+    this.zeroReg = '$0'
+    this.oneReg = '$1'
+    let response = [];
+    response.push(new RegisterEmbed(`xor`, [this.zeroReg, this.zeroReg, this.zeroReg]))
+    response.push(new RegisterEmbed(`addi`, [this.oneReg, this.zeroReg, 1]))
+    for(let i = 0; i < this.asm.length; i++) {
+      const params = this.asm[i].params;
+      switch(this.asm[i].type.toLowerCase()) {
+        case 'mov': {
+          response.push(this._registerMove(params))
+          break;
+        }
+        case 'add': {
+          response.push(new RegisterEmbed('add', [`$${params[0]}`, `$${params[1]}`, `$${params[2]}`]))
+          break;
+        }
+        case 'j': {
+          response.push(new RegisterEmbed('j', params))
+          break;
+        }
+        case 'mul': {
+          response.push(new RegisterEmbed('mul', [`$${params[0]}`, `$${params[1]}`, `$${params[2]}`]))
+          break;
+        }
+        case 'jre': {
+          response.push(new RegisterEmbed('j', [params[0] + i + 1]))
+          break;
+        }
+        case 'benq': {
+          response.push(new RegisterEmbed('bne', [`$${params[0]}`, `$${params[1]}`, params[2]]))
+          break;
+        }
+        case 'beq': {
+          response.push(new RegisterEmbed('beq', [`$${params[0]}`, `$${params[1]}`, params[2]]))
+          break;
+        }
+        case 'slt': {
+          response.push(new RegisterEmbed('slt', [`$${params[0]}`, `$${params[1]}`, `$${params[2]}`]))
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    }
+    return response;
+  }
+}
+
+module.exports = { Register, RegisterEmbed, RegistersEmbed };
